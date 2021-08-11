d11766c2cba0b3184964e65fc7c51190b9fc966d
Jacob Chvatal <jake@isnt.online>
2021-08-11 13:25:24 -0700

    progress on css generator

--- cursor-circle.js
+++ cursor-circle.js
@@ -14,6 +14,46 @@ function circularCursor() {
     );
   }
 
+
+  const css = {
+    "#test-tag": {
+      borderRadius: 3,
+      position: "absolute",
+    }
+  }
+
+  // convert a js object representing a stylesheet to a css string for the sheet
+  function objToCss(obj) {
+    return Object.keys(obj).reduce((currCss, attr) => {
+      let keyStyles = Object.keys(obj[attr]).reduce((currStyle, cssTag) =>
+        `${currStyle ? currStyle + '\n' : ''}  ${dashCase(cssTag)}: ${obj[attr][cssTag]};`, false);
+      return `${currCss ? currCss + '\n' : ''}${dashCase(attr)} {\n${keyStyles}\n}`;
+    }, "");
+  }
+
+  console.log(objToCss(css));
+
+  // determine whether a string is dash-cased
+  function isDashCase(str) {
+    // vestigial regex:
+    // [[a-z]|-]*\w+
+    // return !(/^[A-Z]/.test(word));
+    // a dash case string simply does not contain capital letters
+   return word === word.toLowerCase();
+  }
+
+  // convert a dash-delimited string to camelCase
+  function camelCase(str) {
+    return input.toLowerCase().replace(/-(.)/g, function(match, group1) {
+        return group1.toUpperCase();
+    });
+  }
+
+  // convert a camelCase string to dash-case
+  function dashCase(str) {
+    return str.toLowerCase().replace(/([A-Z])/g, (match, group1) => `-${group1.toLowerCase()}`);
+  }
+
   // Your CSS as text
   // https://stackoverflow.com/questions/707565/how-do-you-add-css-with-javascript
   let styles = `
