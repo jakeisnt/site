12ec321c5b59d31b3b81a2759815c884a91bc2aa
Jacob Chvatal <jake@isnt.online>
2021-08-10 22:49:16 -0700

    wrap up title work v1

--- title.js
+++ title.js
@@ -1,92 +1,86 @@
-title_strings = ["JAKEISNT"];
+function shuffleTitle(
+  titleStrings = ["JAKEISNT"],
+	repeatForever = false,
+	numRepeats = 3,
+	speed = 80,
+	inputDelay = 100,
+	extraChars = "★☆✚☯❤☺⚠❁❊✳✩◍░▊▐☰𝍖⌖⌬⍜⍚⎕⎖〪〫❀",
+) {
+	const availableChars = extraChars + Array.from(new Set(titleStrings.join('').split(''))).join('');
 
-const repeat_forever = true;
-const num_repeats = 1;
-const speed = 80;
-const inputDelay = 100;
-const available_chars = "★☆✚☯❤☺⚠❁❊✳✩◍░▊▐☰𝍖⌖⌬⍜⍚⎕⎖〪〫❀" + Array.from(new Set(title_strings.join('').split(''))).join('');
+	let numCycles = 1;
+	let numCorrect = 0;
+	let currMsgIdx = 0;
+	let timer = null;
+	let canContinue = true;
 
-let num_cycles = 1;
-let count_affected = 0;
-let currMsgIdx = 0;
-let timer = null;
-let can_continue = true;
-
-// swap all of the elements in the provided array with one another at random
-function shuffle(arr){
-	for (i=0; i < arr.length; i++){
-		let k = Math.round(Math.random() * (arr.length - i - 1)) + i;
-		temp = arr[i];arr[i]=arr[k];arr[k]=temp;
-	}
-	return arr;
-}
-
-// fill an array with a sequence of characters starting at start
-function fillRange(start, end) {
-  return new Array(end - start + 1).fill().map((item, index) => start + index);
-};
-
-arr = shuffle(fillRange(0, title_strings[currMsgIdx].length));
-sts = new Array(title_strings[currMsgIdx].length);
-
-for (var i=0; i<title_strings[currMsgIdx].length; i++){
-	sts[i] = title_strings[currMsgIdx].charAt(arr[i]);
-}
-
-
-function setChar(k){
-	r = Math.round(Math.random() * available_chars.length);
-	sts[k] = available_chars.charAt(r);
-
-	document.title = sts.join('');
-
-	if (available_chars.charAt(r) == title_strings[currMsgIdx].charAt(k)){
-		stsmsg = "";
-		for (var i=0; i<sts.length; i++) stsmsg += sts[i];
-		document.title = stsmsg;
-		count_affected++;
-		return;
+	// swap all of the elements in the provided array with one another at random
+	function shuffle(arr){
+		for (i=0; i < arr.length; i++){
+			let k = Math.round(Math.random() * (arr.length - i - 1)) + i;
+			temp = arr[i];arr[i]=arr[k];arr[k]=temp;
+		}
+		return arr;
 	}
 
-	setTimeout(() => setChar(k), speed);
-}
+	// fill an array with a sequence of characters starting at start
+	function fillRange(start, end) {
+		return new Array(end - start + 1).fill().map((item, index) => start + index);
+	};
 
-function init(){
-	if (can_continue) {
-		title_strings[currMsgIdx].split('').forEach((char, charIndex) => {
-			msgchar = char;
-			setChar(charIndex);
-		})
+	function setChar(k){
+		r = Math.round(Math.random() * availableChars.length);
+		sts[k] = availableChars.charAt(r);
 
-		can_continue = false;
-		delay = 200;
-	}
+		document.title = sts.join('');
 
-	if (count_affected == title_strings[currMsgIdx].length){
-		if (currMsgIdx == title_strings.length-1){
-			if ((!repeat_forever) && (num_cycles == num_repeats)){
-				clearTimeout(timer);
-				return;
-			}
-			currMsgIdx = 0;
-			num_cycles++;
-		} else {
-			currMsgIdx++;
+		if (availableChars.charAt(r) == titleStrings[currMsgIdx].charAt(k)) {
+			numCorrect++;
+			return;
 		}
 
-		arr = shuffle(fillRange(0, title_strings[currMsgIdx].length));
-		sts = new Array(title_strings[currMsgIdx].length);
-
-		title_strings[currMsgIdx].split('').forEach((_, i, currMsg) => {
-			sts[i] = currMsg[arr[i]];
-		});
-
-		can_continue = true;
-		count_affected = 0;
-		delay = inputDelay;
+		setTimeout(() => setChar(k), speed);
 	}
 
-	timer = setTimeout(init, delay);
+	function init(){
+		if (canContinue) {
+			titleStrings[currMsgIdx].split('').forEach((char, charIndex) => {
+				msgchar = char;
+				setChar(charIndex);
+			})
+
+			canContinue = false;
+			delay = 200;
+		}
+
+		if (numCorrect == titleStrings[currMsgIdx].length){
+			if (currMsgIdx == titleStrings.length-1){
+				if ((!repeatForever) && (numCycles == numRepeats)){
+					clearTimeout(timer);
+					return;
+				}
+				currMsgIdx = 0;
+				numCycles++;
+			} else {
+				currMsgIdx++;
+			}
+
+			canContinue = true;
+			numCorrect = 0;
+			delay = inputDelay;
+		}
+
+		timer = setTimeout(init, delay);
+	}
+
+	arr = shuffle(fillRange(0, titleStrings[currMsgIdx].length));
+	sts = new Array(titleStrings[currMsgIdx].length);
+
+	titleStrings[currMsgIdx].split('').forEach((_, i, currMsg) => {
+		sts[i] = currMsg[arr[i]];
+	});
+
+	init();
 }
 
-init();
+shuffleTitle();
