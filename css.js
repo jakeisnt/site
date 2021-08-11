81d15f920dc0e352c044d4f4e4a864d3eb77bfe1
Jacob Chvatal <jake@isnt.online>
2021-08-11 14:02:37 -0700

    mini ui and cursor libraries!

---
+++ css.js
@@ -0,0 +1,30 @@
+function css(obj) {
+  const cssStr = (() => {
+    switch(typeof obj) {
+    case "string": return obj;
+    default: return objToCss(obj);
+    }
+  })();
+
+  const styleSheet = document.createElement("style");
+  styleSheet.type = "text/css";
+  styleSheet.innerText = styles;
+  document.head.appendChild(styleSheet);
+}
+
+// convert a js object representing a stylesheet to a css string for the sheet
+function objToCss(obj) {
+  return Object.keys(obj).reduce((currCss, attr) => {
+    let keyStyles = Object.keys(obj[attr]).reduce((currStyle, cssTag) =>
+      `${currStyle ? currStyle + '\n' : ''}  ${dashCase(cssTag)}: ${obj[attr][cssTag]};`, false);
+    return `${currCss ? currCss + '\n' : ''}${dashCase(attr)} {\n${keyStyles}\n}`;
+  }, "");
+}
+
+// convert a camelCase string to dash-case
+function dashCase(str) {
+  return str.replace(/([A-Z])/g, (match, group1) => `-${group1.toLowerCase()}`);
+}
+
+
+export default { css };
--- cursor-circle.js
+++ cursor-circle.js
@@ -1,46 +1,15 @@
-function circularCursor() {
-  document.body.onmousemove = function(e) {
-    document.documentElement.style.setProperty (
-      '--x', (
-        e.clientX+window.scrollX
-      )
-      + 'px'
-    );
-    document.documentElement.style.setProperty (
-      '--y', (
-        e.clientY+window.scrollY
-      )
-      + 'px'
-    );
-  }
+import { css } from './css.js';
 
+function create(name, attrs, parent) {
+  let elem = document.createElement(name);
+  elem = { ... elem, ...attrs };
+  (parent ?? document.body).appendChild(elem);
+  return elem;
+}
 
-  const css = {
-    "#test-tag": {
-      borderRadius: 3,
-      position: "absolute",
-    }
-  }
-
-  // convert a js object representing a stylesheet to a css string for the sheet
-  function objToCss(obj) {
-    return Object.keys(obj).reduce((currCss, attr) => {
-      let keyStyles = Object.keys(obj[attr]).reduce((currStyle, cssTag) =>
-        `${currStyle ? currStyle + '\n' : ''}  ${dashCase(cssTag)}: ${obj[attr][cssTag]};`, false);
-      return `${currCss ? currCss + '\n' : ''}${dashCase(attr)} {\n${keyStyles}\n}`;
-    }, "");
-  }
-
-  console.log(objToCss(css));
-
-  // convert a camelCase string to dash-case
-  function dashCase(str) {
-    return str.replace(/([A-Z])/g, (match, group1) => `-${group1.toLowerCase()}`);
-  }
-
-  // Your CSS as text
-  // https://stackoverflow.com/questions/707565/how-do-you-add-css-with-javascript
-  let styles = `
+// Your CSS as text
+// https://stackoverflow.com/questions/707565/how-do-you-add-css-with-javascript
+const styles = `
     #invertedcursor {
       position: absolute;
       width: 40px;
@@ -54,16 +23,26 @@ function circularCursor() {
       mix-blend-mode: difference;
       transition: transform .2s;
     }
-  `
+`
 
-  var styleSheet = document.createElement("style")
-  styleSheet.type = "text/css"
-  styleSheet.innerText = styles
-  document.head.appendChild(styleSheet)
+function circularCursor() {
+  document.body.onmousemove = function(e) {
+    document.documentElement.style.setProperty (
+      '--x', (
+        e.clientX+window.scrollX
+      )
+        + 'px'
+    );
+    document.documentElement.style.setProperty (
+      '--y', (
+        e.clientY+window.scrollY
+      )
+        + 'px'
+    );
+  }
 
-  let cursor = document.createElement("div");
-  cursor.id = "invertedcursor";
-  document.body.appendChild(cursor);
+  css(styles);
+  create("div", {id: "invertedcursor"});
 }
 
 circularCursor();
