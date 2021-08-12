8422a94e96ed9e06ac2d34ca725632d3c3352981
Jacob Chvatal <jake@isnt.online>
2021-08-11 23:07:41 -0700

    stash cursor ideas

--- src/cursor.js
+++ src/cursor.js
@@ -35,14 +35,41 @@ const styles = `
     }
 `
 
+function mouseOverCircle() {
+  const styles = `
+    #mouseover-circle {
+      position: absolute;
+      width: 750px;
+      height: 2200px;
+      background: #fff;
+      border-radius: 50%;
+      top: 0;
+      left: 0;
+      transform: translate(-50%, -50%);
+      z-index: 2;
+      mix-blend-mode: difference;
+      transition: transform .2s;
+      pointer-events: none;
+    }
+  `;
+
+  UI.css(styles);
+  UI.create("div", {id: "mouseover-circle"},
+    UI.create("div"),
+    UI.create("div"),
+    UI.create("div")
+  )();
+}
+
+// just in case i want to register and manage these myself in some way
 function addListener(name, callback) {
   window.addEventListener(name, callback);
 }
 
 function circularCursor() {
   UI.css(styles);
-  const cursor = UI.create("div", {id: "invertedcursor"});
-  UI.create("div", {id: "funcircle"});
+  const cursor = UI.create("div", {id: "invertedcursor"})();
+  UI.create("div", {id: "funcircle"})();
 
   // document and window can both listen for this event;
   // document is higher in the listener hierarchy so it triggers first,
@@ -50,7 +77,13 @@ function circularCursor() {
   addListener("mousemove", (e) => {
     cursor.style.top = e.pageY + "px";
     cursor.style.left = e.pageX + "px";
+
+    // if another component is nearby:
+    // - decrease the size of the cursor
+    // - expand the size of the other object
   });
 }
+
+mouseOverCircle();
 
 if(!Utils.isMobile()) circularCursor();
