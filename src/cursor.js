be5a655fc71433fbc3ebb427aaae1f68c0b534ab
jakeisnt <jakechvatal@gmail.com>
2021-08-12 20:05:35 -0700

    Cursor absorb button finished! (#12)
    * produce classNames from css
    
    * stash cursor ideas
    
    * we have event listeners for this!!
    
    * experiment notes
    
    * get events working
    
    * fix stylesheet
    
    * show ball in bottom right
    
    * slow following cursor
    
    * only cycle title once#
    
    * change mouse behavior on mouseover
    
    * intermediate progress
    
    * finish mouse v1
    
    * improve mouse flow
    
    * minor ux patches
    
    * mid debugging circle issue
    
    * fix some issues with ball over
    
    * cursor moves smoothly, though is off
    
    * margin threw it all off!
    
    * fixed mouseover for links
    
    * finish highlight functionality!

--- src/cursor.js
+++ src/cursor.js
@@ -1,44 +1,117 @@
-// final goal:
-// mimic how this one works! https://mtg-interieur.fr/
-// - small circle leading large cursor
-// - maybe inversion of text under? maybe covering the text?
-// - 'absorbed' into larger bubbles that react when about to click on them!
-
-const styles = `
-    #invertedcursor {
-      position: absolute;
-      width: 50px;
-      height: 50px;
-      background: gray;
-      border-radius: 50%;
-      top: 0;
-      left: 0;
-      transform: translate(-50%, -50%);
-      z-index: 2;
-      transition: transform .2s;
-      pointer-events: none;
-    }
-    #funcircle {
-      position: absolute;
-      width: 750px;
-      height: 2200px;
-      background: #fff;
-      border-radius: 50%;
-      top: 0;
-      left: 0;
-      transform: translate(-50%, -50%);
-      z-index: 2;
-      mix-blend-mode: difference;
-      transition: transform .2s;
-      pointer-events: none;
-    }
-`
-
 let mousedOverCircle = false;
 let circleX = 0;
 let circleY = 0;
 let circleWidth = 0;
 let circleHeight = 0;
+
+function focusElem(elem) {
+  const rect = elem.getBoundingClientRect();
+  const xPos = (rect.right + rect.left) / 2;
+  const yPos = (rect.bottom + rect.top) / 2;
+  const height = rect.bottom - rect.top;
+  const width = rect.right - rect.left;
+  mousedOverCircle = true;
+  circleX = xPos;
+  circleY = yPos;
+  circleWidth = width;
+  circleHeight = height;
+}
+
+function unfocusElem() {
+  mousedOverCircle = false;
+}
+
+// circle that keeps expanding after mouseover until it colors the foreground!!!
+function circularCursor() {
+  const defaultWidth = 50;
+  const defaultHeight = 50;
+
+  const styles = `
+      #invertedcursor {
+        position: absolute;
+        width: ${defaultWidth}px;
+        height: ${defaultHeight}px;
+        background: gray;
+        border-radius: 50%;
+        top: 0;
+        left: 0;
+        transform: translate(-50%, -50%);
+        z-index: -100;
+        transition: transform .2s;
+        pointer-events: none;
+      }
+  `;
+
+  UI.css(styles);
+  const ball = UI.create("div", {id: "invertedcursor"})();
+
+  let mouseX = 0;
+  let mouseY = 0;
+
+  let ballX = 0;
+  let ballY = 0;
+
+  let ballWidth = defaultWidth;
+  let ballHeight = defaultHeight;
+
+  const moveSpeed = 0.08;
+  const growDecSpeed = 0.02;
+
+  function capMax(val, max) {
+    return val > max ? max : val;
+  }
+
+  function capMin(val, max) {
+    return val < max ? max : val;
+  }
+
+  // should finish growing right when the ball gets to the destination!
+  function followCursor() {
+    let distX = (mousedOverCircle ? circleX : mouseX) - ballX;
+    let distY = (mousedOverCircle ? circleY : mouseY) - ballY;
+
+    ballX += distX * moveSpeed;
+    ballY += distY * moveSpeed;
+
+    ball.style.left = ballX + "px";
+    ball.style.top = ballY + "px";
+
+    if(mousedOverCircle) { // if moused over, increase size to enclose elem
+      const maxWidth = circleWidth * 1.2;
+      const maxHeight = circleHeight * 1.2;
+
+      const growSpeedX = Math.abs(moveSpeed / distX * 5);
+      const growSpeedY = Math.abs(moveSpeed / distY * 5);
+
+      if(ballWidth < maxWidth) {
+        ballWidth = capMax(ballWidth + maxWidth * growSpeedX, maxWidth);
+        ball.style.width = ballWidth + "px";
+      }
+      if(ballHeight < maxHeight) {
+        ballHeight = capMax(ballHeight + maxHeight * growSpeedY, maxHeight);
+        ball.style.height = ballHeight + "px";
+      }
+    } else { // if mouse moves off, decrease size to usual
+      if(ballWidth > defaultWidth) {
+        ballWidth = capMin(ballWidth - circleWidth * growDecSpeed, defaultWidth);
+        ball.style.width = ballWidth + "px";
+      }
+      if(ballHeight > defaultHeight) {
+        ballHeight = capMin(ballHeight - circleHeight * growDecSpeed, defaultHeight);
+        ball.style.height = ballHeight + "px";
+      }
+    }
+
+    requestAnimationFrame(followCursor);
+  }
+
+  window.addEventListener("mousemove", e => {
+    mouseX = event.pageX;
+    mouseY = event.pageY;
+  });
+
+  followCursor();
+}
 
 // enable controls that allow disabling css rules across the document: eg disable all shadows on the page
 // (tracks a 'virtual' index of css rules, then uses these to determine what real css rules to remove, where, and when)
@@ -57,110 +130,24 @@ function mouseOverCircle() {
     }
     `;
 
-
   UI.css(mouseover);
   const circ = UI.create("div", {
     id: "mouseover-circle",
     events: {
-      mouseover: (e) => {
-        const rect = circ.getBoundingClientRect();
-        const xPos = (rect.right + rect.left) / 2;
-        const yPos = (rect.bottom + rect.top) / 2;
-        const height = rect.bottom - rect.top;
-        const width = rect.right - rect.left;
-        mousedOverCircle = true;
-        circleX = xPos;
-        circleY = yPos;
-        circleWidth = width;
-        circleHeight = height;
-      },
-      mouseleave: e => { mousedOverCircle = false; }
+      mouseenter: (e) => focusElem(circ),
+      mouseleave: unfocusElem
     },
   }
   )();
 }
 
-// just in case i want to register and manage these myself in some way
-function addListener(name, callback) {
-  window.addEventListener(name, callback);
+function linkListener() {
+  Array.prototype.slice.call(document.links).forEach(link => {
+    link.addEventListener("mouseenter", e => focusElem(link));
+    link.addEventListener("mouseleave", (e) => { mousedOverCircle = false; });
+  })
 }
 
-// if the cursor is over something we can mouse over:
-// - grow the ball following the cursor
-// - make it hide behind the ui element
-// or: make it go behind the ui element, then expand to show right behind it!
-// when our cursor leaves:
-// - reverse the process
-
-
-// circle that keeps expanding after mouseover until it colors the foreground!!!
-
-function circularCursor() {
-  UI.css(styles);
-  const ball = UI.create("div", {id: "invertedcursor"})();
-  UI.create("div", {id: "funcircle"})();
-
-  let mouseX = 0;
-  let mouseY = 0;
-
-  let ballX = 0;
-  let ballY = 0;
-
-  const defaultWidth = 50;
-  const defaultHeight = 50;
-
-  let ballWidth = defaultWidth;
-  let ballHeight = defaultHeight;
-
-  const moveSpeed = 0.08;
-  const growDecSpeed = 0.02;
-
-  // should finish growing right when the ball gets to the destination!
-
-  function followCursor() {
-    let distX = (mousedOverCircle ? circleX : mouseX) - ballX;
-    let distY = (mousedOverCircle ? circleY : mouseY) - ballY;
-
-    ballX = ballX + (distX * moveSpeed);
-    ballY = ballY + (distY * moveSpeed);
-
-    const growSpeed = moveSpeed / distX;
-
-    ball.style.left = ballX + "px";
-    ball.style.top = ballY + "px";
-
-    if(mousedOverCircle) {
-      const maxWidth = (circleWidth + 20);
-      const maxHeight = (circleHeight + 20);
-      if(ballWidth < maxWidth || ballHeight < maxHeight) {
-        ballWidth += (maxWidth * growSpeed);
-        ballHeight += (maxHeight * growSpeed);
-
-        ball.style.width = ballWidth + "px";
-        ball.style.height = ballHeight + "px";
-      }
-    } else if(ballWidth > defaultWidth || ballHeight > defaultHeight) {
-      ballWidth -= (circleWidth * growDecSpeed);
-      ballHeight -= (circleHeight * growDecSpeed);
-
-      ball.style.width = ballWidth + "px";
-      ball.style.height = ballHeight + "px";
-    }
-
-    requestAnimationFrame(followCursor);
-  }
-
-  followCursor();
-
-
-  // document and window can both listen for this event;
-  // document is higher in the listener hierarchy so it triggers first,
-  // though either can perform either functionality
-  addListener("mousemove", (e) => {
-    mouseX = event.pageX;
-    mouseY = event.pageY;
-  });
-}
-
-if(!Utils.isMobile()) circularCursor();
+linkListener();
 mouseOverCircle();
+if(!Utils.isMobile()) circularCursor();
