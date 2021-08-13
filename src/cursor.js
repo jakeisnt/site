291bf58760850eed045df41bffd9efe8c20ca4c3
Jacob Chvatal <jake@isnt.online>
2021-08-12 18:54:23 -0700

    fix some issues with ball over

--- src/cursor.js
+++ src/cursor.js
@@ -1,58 +1,8 @@
-// final goal:
-// mimic how this one works! https://mtg-interieur.fr/
-// - small circle leading large cursor
-// - maybe inversion of text under? maybe covering the text?
-// - 'absorbed' into larger bubbles that react when about to click on them!
-
-const styles = `
-    #invertedcursor {
-      position: absolute;
-      width: 50px;
-      height: 50px;
-      background: gray;
-      border-radius: 50%;
-      top: 0;
-      left: 0;
-      transform: translate(-50%, -50%);
-      z-index: -100;
-      transition: transform .2s;
-      pointer-events: none;
-    }
-`;
-
 let mousedOverCircle = false;
 let circleX = 0;
 let circleY = 0;
 let circleWidth = 0;
 let circleHeight = 0;
-
-// enable controls that allow disabling css rules across the document: eg disable all shadows on the page
-// (tracks a 'virtual' index of css rules, then uses these to determine what real css rules to remove, where, and when)
-function mouseOverCircle() {
-  const mouseover = `
-    #mouseover-circle {
-      position: fixed;
-      width: 80px;
-      height: 80px;
-      background: #000;
-      border-radius: 50%;
-      bottom: 20px;
-      right: 20px;
-      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-      z-index: 5;
-    }
-    `;
-
-  UI.css(mouseover);
-  const circ = UI.create("div", {
-    id: "mouseover-circle",
-    events: {
-      mouseenter: (e) => focusElem(circ),
-      mouseleave: unfocusElem
-    },
-  }
-  )();
-}
 
 function focusElem(elem) {
   const rect = elem.getBoundingClientRect();
@@ -75,6 +25,22 @@ function unfocusElem() {
 
 // circle that keeps expanding after mouseover until it colors the foreground!!!
 function circularCursor() {
+  const styles = `
+      #invertedcursor {
+        position: absolute;
+        width: 50px;
+        height: 50px;
+        background: gray;
+        border-radius: 50%;
+        top: 0;
+        left: 0;
+        transform: translate(-50%, -50%);
+        z-index: -100;
+        transition: transform .2s;
+        pointer-events: none;
+      }
+  `;
+
   UI.css(styles);
   const ball = UI.create("div", {id: "invertedcursor"})();
 
@@ -102,7 +68,8 @@ function circularCursor() {
     ballX = ballX + (distX * moveSpeed);
     ballY = ballY + (distY * moveSpeed);
 
-    const growSpeed = moveSpeed / distX * 5;
+    const growSpeedX = moveSpeed / Math.abs(distX) * 5;
+    const growSpeedY = moveSpeed / Math.abs(distY) * 5;
 
     ball.style.left = ballX + "px";
     ball.style.top = ballY + "px";
@@ -111,16 +78,14 @@ function circularCursor() {
       const maxWidth = circleWidth + 20;
       const maxHeight = circleHeight + 20;
 
-      if(ballWidth < maxWidth || ballHeight < maxHeight) {
-        ballWidth += (maxWidth * growSpeed);
-        ballHeight += (maxHeight * growSpeed);
+      if(ballWidth < maxWidth) ballWidth += maxWidth * growSpeedX;
+      if(ballHeight < maxHeight) ballHeight += maxHeight * growSpeedY;
 
-        ball.style.width = ballWidth + "px";
-        ball.style.height = ballHeight + "px";
-      }
-    } else if(ballWidth > defaultWidth || ballHeight > defaultHeight) {
-      ballWidth -= (circleWidth * growDecSpeed);
-      ballHeight -= (circleHeight * growDecSpeed);
+      ball.style.width = ballWidth + "px";
+      ball.style.height = ballHeight + "px";
+    } else {
+      if(ballWidth > defaultWidth) ballWidth -= (circleWidth * growDecSpeed);
+      if(ballHeight > defaultHeight) ballHeight -= (circleHeight * growDecSpeed);
 
       ball.style.width = ballWidth + "px";
       ball.style.height = ballHeight + "px";
@@ -135,6 +100,34 @@ function circularCursor() {
     mouseX = event.pageX;
     mouseY = event.pageY;
   });
+}
+
+// enable controls that allow disabling css rules across the document: eg disable all shadows on the page
+// (tracks a 'virtual' index of css rules, then uses these to determine what real css rules to remove, where, and when)
+function mouseOverCircle() {
+  const mouseover = `
+    #mouseover-circle {
+      position: fixed;
+      width: 80px;
+      height: 80px;
+      background: #000;
+      border-radius: 50%;
+      bottom: 20px;
+      right: 20px;
+      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+      z-index: 5;
+    }
+    `;
+
+  UI.css(mouseover);
+  const circ = UI.create("div", {
+    id: "mouseover-circle",
+    events: {
+      mouseenter: (e) => focusElem(circ),
+      mouseleave: unfocusElem
+    },
+  }
+  )();
 }
 
 function linkListener() {
