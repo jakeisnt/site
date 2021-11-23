39202c530e8eff7c8c8694125074b86894c2ec04
Jacob Chvatal <jake@isnt.online>
2021-11-23 13:14:00 -0500

    dynamically change media rules

--- src/dark-mode.js
+++ src/dark-mode.js
@@ -1,3 +1,38 @@
+function arrayUpTo(n) {
+  return Array.apply(null, Array(n)).map((_, i) => i);
+}
+
+function nonListMap(ls, fn) {
+  return arrayUpTo(ls.length).map((i) => fn(ls[i]))
+}
+
+function getMediaRule() {
+
+}
+
+// set a CSS media query rule to a particular value
+function setMediaRule(ruleName, ruleProp) {
+  let ss = document.styleSheets[0];
+  let rules = ss.cssRules || ss.rules; // property name is browser dependent
+
+  nonListMap(rules, (rule) => {
+    if (rule instanceof CSSMediaRule) {
+      const [rname, rprop] = rule.media.mediaText.slice(1, ruletext.length - 1).split(": ")
+      if (rname === ruleName) {
+        rule.media.mediaText = "(" + ruleName + ": " + ruleProp + ")"
+      }
+    }
+  })
+}
+
+setMediaRule("prefers-color-scheme", "light")
+
+
+// console.log(document.styleSheets[0].cssRules)
+
+// console.log(document.styleSheets[0].cssRules[0].media)
+
+
 function toggleTheme() {
     // if it's light -> go dark
     if (themeStylesheet.href.includes('light')) {
