c389c935e8ce240b890e748685376f8624e680e6
Jacob Chvatal <jake@isnt.online>
2021-11-23 21:53:50 -0500

    constants, more req things

--- src/og-preview.js
+++ src/og-preview.js
@@ -1,21 +1,29 @@
 const RECV_STATE = 4;
 const STATUS_OK = 200;
 
+const FT_JSON = "application/json";
+const FT_HTML = "text/html";
+
+const REQUEST_TYPES = {
+  GET: "GET",
+}
+
 function parseHTML() {
   return new DOMParser.parseFromString("text/html");
 }
 
-function getPage(type, url, data) {
+function request(type, url, data) {
   return new Promise((resolve, reject) => {
     const req = new XMLHTTPRequest();
+
     req.onreadystatechange = () => {
       if(this.readyState === RECV_STATE) {
         const responseType = req.getResponseHeader('content-type');
         if(this.status === STATUS_OK) {
           // parse different types of data for you
           switch(responseType) {
-            case 'application/json': resolve(JSON.parse(req.responseText));
-            case 'text/html': resolve(parseHTML(req.responseText));
+            case FT_JSON: resolve(JSON.parse(req.responseText));
+            case FT_HTML: resolve(parseHTML(req.responseText));
             default: resolve(req.responseText);
           } else {
             reject({status: req.status, body: req.responseText });
@@ -27,4 +35,10 @@ function getPage(type, url, data) {
     req.open(type, url, true);
     req.send();
   })
+}
+
+function openGraphData(url) {
+  const og_html = request(REQUEST_TYPES.GET, "https://facebook.com", null);
+  // get og tags in header
+  // organize into description, image, etc
 }
