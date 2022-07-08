692e3d5b6b44211c88e22f6ce4c443ed38eb8780
Jacob Chvatal <jake@isnt.online>
2022-07-08 16:24:19 -0400

    regen index

--- src/site/core.clj
+++ src/site/core.clj
@@ -44,9 +44,10 @@
 ; - when converting to dynamic website, play with andreas gyasin's ideas of moving text on the page;
 ;   we want the process to be reversible, and we do not want to pollute this page with any more bloat than necessary
 
-(defn gen-homepage 
-  "Generate a homepage for the website."
-  [info]
+
+(defn webpage
+  "Generate a webpage body thing"
+  [style info body]
   (hp/html5 
     {:lang "en-us"}
     [:style style]
@@ -64,36 +65,44 @@
      [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
      [:meta {:name "theme-color" :content "#fff"}]
      [:link {:rel "manifest" :href "/site.webmanifest"}]]
-    [:body
-     [:main 
-      [:p "Hey! I'm Jake, a student at " [:a {:href "https://northeastern.edu" :target "_blank" :rel "noreferrer"} "NEU"]
-       " interested in user interfaces, programming languages and sustainability."]
+    [:body body]))
 
-      [:p "I believe that everyone deserves the ability to use computing to express themselves. 
-          To this end, I aim to build better interfaces - from web UIs to "[:a {:href "https://github.com/jakeisnt/nixcfg/"} "unix service layers"]" - that improve ways in which people understand and interact with their computers."]
 
-      [:p "I value high impact roles with missions that benefit everyone involved."]
+(defn gen-homepage 
+  "Generate a homepage for the website."
+  [info]
+  (webpage 
+    style
+    info
+    [:main 
+     [:p "Hey! I'm Jake, a student at " [:a {:href "https://northeastern.edu" :target "_blank" :rel "noreferrer"} "NEU"]
+      " interested in user interfaces, programming languages and sustainability."]
 
-      [:p 
-       "At " [:a {:href "https://skira.se"} "Skira"]", I helped democratize "[:br ]" the Scandinavian grain market, " [:br]
-       "and at " [:a {:href "https://theroutingcompany.com"} "TRC"] " I provide equitable access to public transportation." [:br]
-       "Others: " 
-       [:a {:href "https://sandboxnu.com"} "sbox"] ", "
-       [:a {:href "https://psu.edu"} "psu"] ", "
-       [:a {:href "https://intel.com"} "intel"] ", "
-       [:a {:href "https://cdkglobal.com"} "cdk"] "." [:br]
-       "More details on " [:a {:href (:cv info)} "my full CV"] "."]
+     [:p "I believe that everyone deserves the ability to use computing to express themselves. 
+         To this end, I aim to build better interfaces - from web UIs to "[:a {:href "https://github.com/jakeisnt/nixcfg/"} "unix service layers"]" - that improve ways in which people understand and interact with their computers."]
 
-      [:p 
-       "Check out my " [:a {:href "https://wiki.jacob.chvatal.com"} "notes"] " and "
-       [:a {:href (str "https://github.com/" (:github info))} "code"] "!" [:br]
+     [:p "I value high impact roles with missions that benefit everyone involved."]
 
-       "I'm on " [:a {:href (str "https://twitter.com/" (:twitter info))} "twitter"] " and " 
-       [:a {:href (str "https://www.instagram.com/" (:instagram info))} "instagram"] "." [:br]
-       [:a {:href "mailto:jake@isnt.online" :target "_blank"} "Email"] " or " [:a {:href (str "sms://" (:phone info))} "text"] " me if you'd like."]
+     [:p 
+      "At " [:a {:href "https://skira.se"} "Skira"]", I helped democratize "[:br ]" the Scandinavian grain market, " [:br]
+      "and at " [:a {:href "https://theroutingcompany.com"} "TRC"] " I provide equitable access to public transportation." [:br]
+      "Others: " 
+      [:a {:href "https://sandboxnu.edu"} "sbox"] ", "
+      [:a {:href "https://psu.edu"} "psu"] ", "
+      [:a {:href "https://intel.com"} "intel"] ", "
+      [:a {:href "https://cdkglobal.com"} "cdk"] "." [:br]
+      "More details on " [:a {:href (:cv info)} "my full CV"] "."]
 
-      [:p "Chat soon," [:br] (:name info)]]
-[:div
+     [:p 
+      "Check out my " [:a {:href "https://wiki.jacob.chvatal.com"} "notes"] " and "
+      [:a {:href (str "https://github.com/" (:github info))} "code"] "!" [:br]
+
+      "I'm on " [:a {:href (str "https://twitter.com/" (:twitter info))} "twitter"] " and " 
+      [:a {:href (str "https://www.instagram.com/" (:instagram info))} "instagram"] "." [:br]
+      [:a {:href "mailto:jake@isnt.online" :target "_blank"} "Email"] " or " [:a {:href (str "sms://" (:phone info))} "text"] " me if you'd like."]
+
+     [:p "Chat soon," [:br] (:name info)]
+     [:div
       [:div {:style "display: flex; flex-direction: row;"}
        [:a.foot {:href "https://creativecommons.org/licenses/by-nc-sa/4.0" :target "_blank" :rel "noreferrer"} "[cc]"]
        [:a.foot {:href "./jakeisnt.asc"} "[pgp]"]
