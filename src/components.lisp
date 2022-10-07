458e5c6d41147b06b5c0e0d026b2997951a4c2a9
Jacob Chvatal <jake@isnt.online>
2022-10-07 10:07:53 -0400

    design notes

--- src/components.lisp
+++ src/components.lisp
@@ -21,15 +21,23 @@
            :onclick (parenscript:ps (toggle-hypothesis))
            "hypothes.is"))))
 
-;; ("p" "filename")
-;; ("filename")
-;; ("a" "p" "filename")
+;; () -> (:a :href jake/indexx)
+;; ("filename") -> (:a :href jake/filename)
+;; ("p" "filename") -> (:a :href jake/p/index) (:a :href jake/p/filename)
+;; ("a" "p" "filename")  -> (:a :href jake/a/index) (:a :href jake/a/p/index) (:a :href jake/a/p/filename)
+
+;; i want this site to feel like the navigation of a file system;
+;; view version control, depth, what's in current dir;
+;; support files of all types;
+;; ideally this just doubles as a source code viewer, honestly
+;; should mousing over reveal alternative things for this path? is that right?
 
 (defmacro make-pathlist (ls)
   `(loop for pathpart in ls
          collect (:a :href (concatenate 'string root "/" pathpart) )
          )
   )
+
 
 
 (defun sidebar (pathlist)
