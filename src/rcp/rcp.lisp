1b7f80503028488f742cfda9d30933839a830b51
Jacob Chvatal <jake@isnt.online>
2022-11-07 09:47:10 -0500

    some recipe website ideas

--- src/rcp/rcp.lisp
+++ src/rcp/rcp.lisp
@@ -1,1 +1,18 @@
 ;; recipe file specification
+
+;; ingredients
+
+;; instructions
+
+;; features:
+;; - checkboxes
+;; - timers
+;; - how do we model instructions in parallel? works like a graph?
+
+;; future:
+;; - calories
+;; - nutrition
+;; - aggregated data (time estimates, servings, cook time, prep time)
+
+
+;; ingredients | preparation steps
