1527636c71441c0dab0ead1184b2f4a1eb78fd50
Jacob Chvatal <jake@isnt.online>
2023-02-22 15:40:27 -0500

    git status  working

--- src/main.clj
+++ src/main.clj
@@ -1,8 +1,19 @@
 (ns main
-  (:require [org-parser.parser :refer [parse]]
-            [org-parser.core :refer [read-str write-str]]))
+  (:require
+   ;; TODO: Why doesn't 'parse' work? Why can't it be found?
+   ;; [org-parser.parser :refer [parse]]
+   [org-parser.core :refer [read-str write-str]]
+   [hiccup.core :refer [html]]
+   [clj-jgit.porcelain :as git]))
 
-(defn run [opts]
-  (prn (parse "* Headline"))
+;; TODO
+(defn -main [opts]
+  ;; (prn (parse "* Headline"))
   (prn (read-str "* Headline"))
   (println (write-str (read-str "* Headline"))))
+
+(defn main2 [opts]
+  (prn "hello world"))
+
+(def my-repo (git/load-repo "/home/jake/site"))
+(git/git-status my-repo)
