9a57a03761c42f8341c6bf0a22c8f4c6093a06e6
Jacob Chvatal <jake@isnt.online>
2023-02-22 15:18:19 -0500

    not pulling in dpes

--- src/main.clj
+++ src/main.clj
@@ -2,6 +2,7 @@
   (:require [org-parser.parser :refer [parse]]
             [org-parser.core :refer [read-str write-str]]))
 
-(prn (parse "* Headline"))
-(prn (read-str "* Headline"))
-(println (write-str (read-str "* Headline")))
+(defn run [opts]
+  (prn (parse "* Headline"))
+  (prn (read-str "* Headline"))
+  (println (write-str (read-str "* Headline"))))
