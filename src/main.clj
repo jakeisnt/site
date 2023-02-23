ccc7eb3f5ff70896a395d08069f002e5e02fc292
Jacob Chvatal <jake@isnt.online>
2023-02-23 00:05:55 -0500

    git history is back

--- src/main.clj
+++ src/main.clj
@@ -3,7 +3,48 @@
    [cybermonday.core :as cm]
    [clojure.string :as str]
    [hiccup.core :as h]
-   [clojure.java.io :as io]))
+   [clojure.java.io :as io]
+   [clojure.java.shell :as sh]))
+
+;; clojure git configuration
+;; https://gist.github.com/rnesytov/b944bf9f681b0c42519cd0b6ab8f44e5
+(defn run-command [command & {:keys [directory] :or {directory (System/getProperty "user.dir")}}]
+  (sh/with-sh-dir directory
+    (sh/sh "sh" "-c" command)))
+
+(defn resolve-command [command & [args]]
+  (reduce
+   (fn [command arg-key]
+     (str/replace command (str "%" (name arg-key)) (arg-key args)))
+   command
+   (keys args)))
+
+(defn wrapper [command path & [args]]
+  (let [result (run-command (resolve-command command args) :directory path)]
+    (if (zero? (:exit result))
+      (:out result)
+      false)))
+
+;; TODO: Get git history of org-mode files as well.
+(defn last-modified [file]
+  (str/split
+   (wrapper (str "git log --follow --format=%ad --date default --date=format:'%Y-%m-%d' ./" file) "/home/jake/wiki")
+   #"\R"))
+
+(defn swap-extension [file]
+  (str/replace file #"\.html$" ".md"))
+
+;; ASSUMES file is in relative path to wiki repo
+(defn git-log [file]
+  (let [res (wrapper (str "git log --all --full-history --pretty=\"format:%h %H %ad\" --date default --date=format:'%Y-%m-%d' " "/home/jake/wiki/" file) "/home/jake/wiki")]
+    (if res
+      (for [line (str/split res #"\R")]
+        (let [[short-hash long-hash commit-date] (str/split line #" ")]
+          {:short-hash short-hash
+           :long-hash long-hash
+           :commit-date commit-date
+           :file-path file}))
+      (throw (Throwable. (str "git log command failed on path " file))))))
 
 (def site-name "Jake Chvatal")
 (def source-dir "/home/jake/wiki/pages")
@@ -13,7 +54,7 @@
   "Get all the files in a directory"
   [dir]
   (let [directory (clojure.java.io/file dir)]
-    ;; file-seq includes the dir itself
+    ;; file-seq consistently includes the dir itself as the first argument
     (rest (file-seq directory))))
 
 (defn read-file [path]
@@ -63,6 +104,20 @@
        [:a {:href "/"} "jake."])
      [:a {:href "https://isnt.online"} " ~ "]
      (collect-folder-paths path-list title)]))
+
+(defn history-link [long-hash file-path]
+  (str "https://github.com/jakeisnt/wiki/blob/" long-hash "/" file-path))
+
+(defn git-history-table [file-path]
+  (let [file-path (swap-extension (remove-path-prefix file-path))
+        git-log (git-log file-path)]
+    [:div.git-hist-table
+     [:table
+      [:tbody
+       (for [commit git-log]
+         [:tr
+          [:td (:commit-date commit)]
+          [:td [:a {:href (history-link (:long-hash commit) (:file-path commit))} (:short-hash commit)]]])]]]))
 
 (defn filename
   "Get the file name, without the extension, from a path"
@@ -120,8 +175,7 @@
          [:article.wikipage
           (when (not has-title) [:h1.title-top page-name])
           md-article]]
-        ;; TODO git history table
-        [:div.git-history-table]]]])))
+        (git-history-table target-path)]]])))
 
 (defn change-path [path]
   ;; TODO: Remove hard-coded paths.
@@ -131,7 +185,6 @@
 
 (defn transform-file [path]
   (let [target-path (change-path path)]
-    (println (str "Transforming " path))
     (->
      path
      read-file
