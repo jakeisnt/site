703b0952afd73f68a1be97efb9faaef3d80f837c
Jacob Chvatal <jake@isnt.online>
2023-02-23 20:06:04 -0500

    main page rewritten (calendar aside)

--- src/main.clj
+++ src/main.clj
@@ -169,6 +169,41 @@
                 [:td.file-type-row (file/extension file)]
                 [:td (:commit-date (first log))]]))]]]]]])))
 
+(defn no-js []
+  [:noscript
+   [:div.git-hist-table
+    [:h3 "Thank you for disabling javascript."]
+    [:p "This website is augmented with JS, but is perfectly functional without it. The web should be usable with static files alone."]]])
+
+(defn lastfm-now-playing []
+  [:span
+   [:link {:rel "stylesheet" :href "/lastfm.css"}]
+   [:script {:src "/lfmNowPlaying.js"}]
+   [:script "getNowPlaying();"]])
+
+(defn link-info-table []
+  [:table
+   [:tr [:td "Mastodon"] [:td [:a {:href "https://merveilles.town/@jakeisnt"} "jakeisnt"]]]
+   [:tr [:td "Twitter"] [:td [:a {:href "https://twitter.com/jakeissnt"} "jakeissnt"]]]
+   [:tr [:td "GitHub"] [:td [:a {:href "https://github.com/jakeisnt"} "jakeisnt"]]]
+   [:tr [:td "Email"] [:td [:a {:href "mailto://jake+website@isnt.online"} "jake @ ~"]]]])
+
+(defn home-page []
+  (h/html
+   [:html
+    (head "Jake Chvatal")
+    [:body
+     [:div#site-body.site-body
+      (sidebar "/" "index")
+      [:main
+       [:article.wikipage
+        [:p "Hey, I'm Jake."]
+        [:p "I like old tools and simple ideas."]
+        [:p "Check out my " [:a.external {:href "/pages/index.html"} "notes"] " for more."]]
+       (no-js)
+       (lastfm-now-playing)
+       (link-info-table)]]]]))
+
 (defn render-article [md-article source-path target-path]
   (let [page-name (get-page-name md-article target-path)
         has-title (has-title? md-article)]
@@ -205,6 +240,7 @@
 
 (defn -main [_]
   (let [files (file/list pages-source-dir)]
+    (file/write (home-page) (str target-dir "/index.html"))
     (make-dir pages-source-dir)
     (doseq [file files]
       (transform-file file))))
