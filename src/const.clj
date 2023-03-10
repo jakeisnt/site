69bfb6463777358c0016601ed734e76304792c4c
Jacob Chvatal <jake@isnt.online>
2023-03-10 00:58:00 -0500

    index: Better Names

--- src/const.clj
+++ src/const.clj
@@ -17,9 +17,9 @@
    {:name "Gitlab" :url "https://gitlab.com/jakeisnt" :user "jakeisnt"}
    {:name "sourcehut" :url "https://sr.ht/~jakeisnt/" :user "jakeisnt"}
    {:name "YouTube" :url "https://www.youtube.com/@jakeisnt" :user "jakeisnt"}
-   {:name "Maps" :url "https://www.google.com/maps/contrib/109731430420919295575/" :user "Jake Chvatal"}
+   {:name "Google Maps" :url "https://www.google.com/maps/contrib/109731430420919295575/" :user "Jake Chvatal"}
    {:name "Email" :url "mailto://jake+website@isnt.online" :user "jake @ ~"}
-   {:name "OSM" :url "https://www.openstreetmap.org/user/jakeisnt" :user "jakeisnt"}
+   {:name "OpenStreetMaps" :url "https://www.openstreetmap.org/user/jakeisnt" :user "jakeisnt"}
    {:name "Hacker News" :url "https://news.ycombinator.com/user?id=jakeisnt" :user "jakeisnt"}
    {:name "Instagram" :url "https://instagram.com/jakeisnt" :user "jakeisnt"}
    {:name "read.cv" :url "https://read.cv/jakeisnt" :user "jakeisnt"}
