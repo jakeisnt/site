1338eb5d03d8ff185112e60a83518b3843b593e8
Jacob Chvatal <jake@isnt.online>
2023-04-11 02:57:12 +0200

    space out links, add 'deprecated'

--- src/const.clj
+++ src/const.clj
@@ -11,20 +11,49 @@
 (def local-port 4242)
 
 (def profiles
-  [{:name "Are.na" :url "https://are.na/jake-isnt" :user "jake-isnt"}
-   {:name "Mastodon" :url "https://merveilles.town/@jakeisnt" :user "jakeisnt"}
-   {:name "Twitter" :url "https://twitter.com/jakeissnt" :user "jakeissnt"}
-   {:name "GitHub" :url "https://github.com/jakeisnt" :user "jakeisnt"}
-   {:name "Gitlab" :url "https://gitlab.com/jakeisnt" :user "jakeisnt"}
-   {:name "sourcehut" :url "https://sr.ht/~jakeisnt/" :user "jakeisnt"}
-   {:name "YouTube" :url "https://www.youtube.com/@jakeisnt" :user "jakeisnt"}
-   {:name "Google Maps" :url "https://www.google.com/maps/contrib/109731430420919295575/" :user "Jake Chvatal"}
-   {:name "Email" :url "mailto://jake+website@isnt.online" :user "jake @ ~"}
-   {:name "OpenStreetMaps" :url "https://www.openstreetmap.org/user/jakeisnt" :user "jakeisnt"}
-   {:name "Hacker News" :url "https://news.ycombinator.com/user?id=jakeisnt" :user "jakeisnt"}
-   {:name "Instagram" :url "https://instagram.com/jakeisnt" :user "jakeisnt"}
-   {:name "read.cv" :url "https://read.cv/jakeisnt" :user "jakeisnt"}
-   {:name "LinkedIn" :url "https://www.linkedin.com/in/jacob-chvatal" :user "jacob-chvatal"}])
+  [{:name "Are.na"
+    :url "https://are.na/jake-isnt"
+    :user "jake-isnt"}
+   {:name "Mastodon"
+    :url "https://merveilles.town/@jakeisnt"
+    :user "jakeisnt"}
+   {:name "Twitter"
+    :url "https://twitter.com/jakeissnt"
+    :user "jakeissnt"}
+   {:name "GitHub"
+    :url "https://github.com/jakeisnt"
+    :user "jakeisnt"}
+   {:name "Gitlab"
+    :url "https://gitlab.com/jakeisnt"
+    :user "jakeisnt"}
+   {:name "sourcehut"
+    :url "https://sr.ht/~jakeisnt/"
+    :user "jakeisnt"}
+   {:name "YouTube"
+    :url "https://www.youtube.com/@jakeisnt"
+    :user "jakeisnt"}
+   {:name "Email"
+    :url "mailto://jake+website@isnt.online"
+    :user "jake @ ~"}
+   {:name "Google Maps"
+    :url "https://www.google.com/maps/contrib/109731430420919295575/"
+    :user "Jake Chvatal"}
+   {:name "OpenStreetMaps"
+    :url "https://www.openstreetmap.org/user/jakeisnt"
+    :user "jakeisnt"}
+   {:name "Hacker News"
+    :url "https://news.ycombinator.com/user?id=jakeisnt"
+    :user "jakeisnt"}
+   {:name "Instagram"
+    :url "https://instagram.com/jakeisnt"
+    :user "jakeisnt"}
+   {:name "read.cv"
+    :url "https://read.cv/jakeisnt"
+    :user "jakeisnt"}
+   {:name "LinkedIn"
+    :url "https://www.linkedin.com/in/jacob-chvatal"
+    :user "jacob-chvatal"
+    :deprecated true}])
 
 (def personal
   {:name "Jake Chvatal"
