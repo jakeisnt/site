b4ce851d30a1fc2aad4bf368bc87bb0a8fdf57de
Jacob Chvatal <jake@isnt.online>
2023-04-22 22:02:15 +0200

    move deployment out to new file

---
+++ src/deploy.clj
@@ -0,0 +1,51 @@
+(ns deploy
+  (:require file markdown const path act home index git
+            [main]))
+
+(defn commit-folder-to [v]
+  (let [repo (:repo v)
+        deployment-dir (:deployment-dir v)
+        branch (:branch v)
+        current-branch (git/current-branch repo)
+        tmp-dir "/tmp/jake-site-deploy"]
+
+    (println "copying deployment to tmp dir")
+    (git/checkout repo branch)
+    (file/move deployment-dir tmp-dir repo)
+    (git/status repo)
+
+    (println "removing all untracked files")
+    (git/remove-untracked repo)
+    (git/status repo)
+
+    (println "moving tmp dir contents to root")
+    (file/copy-dir tmp-dir deployment-dir repo)
+    (file/copy-dir (str deployment-dir "/*") "." repo)
+    (git/status repo)
+
+    (println "pushing build")
+    (println "we are on branch " (git/current-branch repo))
+    (git/add-all repo)
+    (git/commit repo)
+    (git/push repo)
+    (git/status repo)
+
+    (println "restoring working branch")
+    (git/checkout repo current-branch)
+    (git/status repo)
+
+    (print "removing untracked")
+    (git/remove-untracked repo)
+    (git/status repo)
+
+    (print "removing deployment dir")
+    (file/remove-dir deployment-dir repo)
+    (print "moving tmp dir to deployment dir")
+    (file/move tmp-dir deployment-dir repo)))
+
+(defn -main [_]
+  (main/-main nil)
+  (file/copy-force (str const/current-repo "/resources/*") const/target-dir const/current-repo)
+  (commit-folder-to {:repo const/current-repo
+                     :branch const/deployment-branch
+                     :deployment-dir const/target-dir}))
--- src/main.clj
+++ src/main.clj
@@ -26,7 +26,6 @@
           (match (file/extension source-path)
             "md"  (markdown/->file source-path target-path)
             "act" (act/->file source-path target-path)))))))
-;;
 
 (defn make-dir
   "Make a directory listing page"
@@ -50,54 +49,6 @@
   (doseq [path const/wiki-paths]
     (write-path path))
   (record-last-timestamp const/source-dir))
-
-(defn commit-folder-to [v]
-  (let [repo (:repo v)
-        deployment-dir (:deployment-dir v)
-        branch (:branch v)
-        current-branch (git/current-branch repo)
-        tmp-dir "/tmp/jake-site-deploy"]
-
-    (println "copying deployment to tmp dir")
-    (git/checkout repo branch)
-    (file/move deployment-dir tmp-dir repo)
-    (git/status repo)
-
-    (println "removing all untracked files")
-    (git/remove-untracked repo)
-    (git/status repo)
-
-    (println "moving tmp dir contents to root")
-    (file/copy-dir tmp-dir deployment-dir repo)
-    (file/copy-dir (str deployment-dir "/*") "." repo)
-    (git/status repo)
-
-    (println "pushing build")
-    (println "we are on branch " (git/current-branch repo))
-    (git/add-all repo)
-    (git/commit repo)
-    (git/push repo)
-    (git/status repo)
-
-    (println "restoring working branch")
-    (git/checkout repo current-branch)
-    (git/status repo)
-
-    (print "removing untracked")
-    (git/remove-untracked repo)
-    (git/status repo)
-
-    (print "removing deployment dir")
-    (file/remove-dir deployment-dir repo)
-    (print "moving tmp dir to deployment dir")
-    (file/move tmp-dir deployment-dir repo)))
-
-(defn -deploy [_]
-  (-main nil)
-  (file/copy-force (str const/current-repo "/resources/*") const/target-dir const/current-repo)
-  (commit-folder-to {:repo const/current-repo
-                     :branch const/deployment-branch
-                     :deployment-dir const/target-dir}))
 
 (comment (write-home))
 (comment (make-dir (str const/source-dir "/" "scripts") (str const/target-dir "/" "scripts")))
