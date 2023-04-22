d3ded09c2b8b6b7d79674a8170e955e802ea5613
Jacob Chvatal <jake@isnt.online>
2023-04-22 13:30:55 +0200

    sort files by name

--- src/index.clj
+++ src/index.clj
@@ -1,6 +1,14 @@
 (ns index
   (:require file html const git path))
 
+(defn get-file-info [file]
+  (let [log (git/log (file/path file) const/source-dir)]
+    {:file file :log log :newest-log (first log) :name (file/name file)}))
+
+(defn sort-files [files]
+  (let [file-list (for [file files] (get-file-info file))]
+    (sort-by :name file-list)))
+
 (defn html [source-path target-path]
   (let [title (file/name source-path)
         files (file/list source-path)]
@@ -13,13 +21,12 @@
         [:main
          [:div.folder-index-page-table
           [:table
-           (for [file files]
-             (let [log (git/log (file/path file) const/source-dir)]
-               [:tr
-                [:td (:short-hash (first log))]
-                [:td.file-name-tr [:a {:href (path/->html (path/->url (file/path file)))} (file/name file)]]
-                [:td.file-type-row (file/extension file)]
-                [:td (:commit-date (first log))]]))]]]]]])))
+           (for [file (sort-files files)]
+             [:tr
+              [:td (:short-hash (:newest-log file))]
+              [:td.file-name-tr [:a {:href (path/->html (path/->url (file/path (:file file))))} (:name file)]]
+              [:td.file-type-row (file/extension (:file file))]
+              [:td (:commit-date (:newest-log file))]])]]]]]])))
 
 (defn ->file [source-path target-path]
   (-> (html source-path target-path)
