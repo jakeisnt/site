0d8edb2f2f810e35a2246901006d4ef6a3ecdc40
Jacob Chvatal <jake@isnt.online>
2023-04-22 21:01:38 +0200

    robot commit

---
+++ docs/docs/docs/lib.js
@@ -0,0 +1,17 @@
+function print(...txt) {
+    return console.log(txt);
+};
+
+function runOnDesktop(fn) {
+    if (window.matchMedia("(min-width: 500px)")) {
+        return fn();
+    }
+}
+
+function dynLoad(src, id) {
+    var s = document.createElement('script');
+    s.setAttribute('src', src);
+    s.setAttribute('id', id);
+    s.setAttribute('async', true);
+    return s;
+};
---
+++ docs/docs/docs/toggle-dark-mode.js
@@ -0,0 +1,28 @@
+/* A script to toggle dark mode with a button,
+ * storing the current state in browser local storage.
+ * The button hasn't yet been added...
+ */
+const btn = document.querySelector(".btn-toggle");
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme == "dark") {
+  document.body.classList.toggle("dark-theme");
+} else if (currentTheme == "light") {
+  document.body.classList.toggle("light-theme");
+}
+
+btn.addEventListener("click", function () {
+  if (prefersDarkScheme.matches) {
+    document.body.classList.toggle("light-theme");
+    var theme = document.body.classList.contains("light-theme")
+      ? "light"
+      : "dark";
+  } else {
+    document.body.classList.toggle("dark-theme");
+    var theme = document.body.classList.contains("dark-theme")
+      ? "dark"
+      : "light";
+  }
+  localStorage.setItem("theme", theme);
+});
