c9989769d6a890b7b58def538082d6cb7d2e3360
Jacob Chvatal <jake@isnt.online>
2023-04-23 19:39:58 +0200

    terminal ideas

--- resources/components/terminal/terminal.js
+++ resources/components/terminal/terminal.js
@@ -1,3 +1,12 @@
+// ideas:
+// - browse files available
+// - open new windows with animations
+// - open files in new windows
+// - use functions remotely
+// - control things in my real life
+// - browse/analyze/visualize my personal data
+
+// biggest problem: this thing goes away when i browse to another page
 $('.terminal').terminal(function(command, term) {
   term.pause();
   term.echo("I can't do much yet.").resume();
