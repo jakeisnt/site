6b6629e3440b25cc4819341a0e65e2d3e301d4ed
Jacob Chvatal <jake@isnt.online>
2023-05-24 21:19:53 +0200

    robot commit

--- elements.js
+++ elements.js
@@ -3,14 +3,27 @@ const codeBlocks = document.getElementsByTagName('code');
 console.log(codeBlocks);
 
 Array.from(codeBlocks).forEach((codeBlock) => {
-    const btn = document.createElement('button');
-    btn.classList.add('inset');
-    btn.innerText = 'Copy';
-    codeBlock.appendChild(btn);
+  const btn = document.createElement('button');
+  btn.classList.add('inset');
+  btn.innerText = 'Copy';
+  codeBlock.parentElement.style = 'position: relative';
+  codeBlock.parentElement.appendChild(btn);
 
-    btn.onclick = function(e) {
-      const codeContents = e.target.parentElement.innerText;
-      const code = codeContents.substring(0, codeContents.length - e.target.innerText.length);
-      navigator.clipboard.writeText(code);
-    };
+  const label = document.createElement('div');
+  label.classList.add('label');
+  label.classList.add('inset')
+  label.innerText = codeBlock.classList[0].replace('language-', '');
+  codeBlock.parentElement.appendChild(label);
+
+  btn.onclick = function(e) {
+    const codeContents = e.target.parentElement.innerText;
+    const code = codeContents.substring(0, codeContents.length - e.target.innerText.length);
+    navigator.clipboard.writeText(code);
+  };
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  document.querySelectorAll('pre code').forEach((block) => {
+    hljs.highlightElement(block);
+  });
 });
