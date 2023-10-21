0378117409dfdc3eee9d05a44d675d68840f724e
Jacob Chvatal <jake@isnt.online>
2023-10-21 23:57:11 +0200

    spec out file/index.js

---
+++ src/file/index.js
@@ -0,0 +1,4 @@
+// this file should be a standard interface for interacting with files.
+//
+// you should be able to create a file with a path string
+// and the specific file class should handle the rest.
