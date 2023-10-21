c43a26fd7380c4bfc911423df82629d35186d01d
Jacob Chvatal <jake@isnt.online>
2023-10-21 21:47:17 +0200

    implement half of the html header file

--- src/html/html.clj
+++ src/html/html.clj
@@ -9,27 +9,6 @@
 ;; delimits paths in title and in body
 (def ^:const path-delimiter " / ")
 
-(defn metm [k v]
-  [:meta {:name k :content v}])
-
-(defn prop [k v]
-  [:meta {:property k :content v}])
-
-(defn defer-script
-  ([src] [:script {:src src :id src :defer true}]))
-
-(defn script
-  ([src] [:script {:src src :id src}])
-  ([src body] [:script {:src src :id src} body]))
-
-(defn css
-  ([href] [:link {:rel "stylesheet" :type "text/css" :href href :id href}])
-  ([href body] [:link {:rel "stylesheet" :type "text/css" :href href :id href} body]))
-
-(defmacro favicon []
-  [:link {:rel "icon" :type "image/x-icon" :href "/favicon/favicon.ico"}]
-  [:link {:rel "apple-touch-icon" :href "/favicon/apple-touch-icon.png"}])
-
 (defn make-path-list [path]
   (path/split (path/remove-prefix path const/target-dir)))
 
@@ -38,42 +17,6 @@
     title
     (str (collect-folder-paths-string (rest path-list) title) path-delimiter (first path-list))))
 
-(defn head
-  "A page header that works for all pages"
-  [path title]
-  [:head
-   [:meta {:charset "UTF-8"}]
-   [:title (str (collect-folder-paths-string (make-path-list path) title) path-delimiter const/site-name)]
-   (metm "viewport" "width=device-width,initial-scale=1.0")
-   (prop "og:title" title)
-   (prop "og:type" "website")
-   (prop "og:url" const/target-url)
-   (prop "og:site_name" const/site-name)
-   (metm "description" "hi") ;; TODO
-   ;; TODO pull this information from the articles
-   (metm "keywords" "Operating Systems, webring, programming, languages")
-   (metm "author" "Jake Chvatal")
-   (metm "robots" "index,follow")
-
-   ;; TODO: keep this synced with global theme somehow?
-   [:meta {:name "theme-color" :media "(prefers-color-scheme: light)" :content "white"}]
-   [:meta {:name "theme-color" :media "(prefers-color-scheme: dark)" :content "#111"}]
-   (favicon)
-
-   [:link {:rel "manifest" :href "/manifest.json"}]
-
-   (css "/resources/style.css")
-   (css "/resources/global.css")
-   (script "/resources/lib.js")
-
-   (css "/resources/elements.css")
-   (defer-script "/resources/elements.js")
-
-   ;; TODO: move to code block enhancements
-   (defer-script "https://unpkg.com/@highlightjs/cdn-assets@11.7.0/highlight.min.js")
-   (defer-script "https://unpkg.com/@highlightjs/cdn-assets@11.7.0/languages/clojure.min.js")
-   [:link {:rel "stylesheet" :id "dark-theme-highlight" :href "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/nord.min.css"}]
-   [:link {:rel "stylesheet" :id "light-theme-highlight" :href "https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/atom-one-light.min.css"}]])
 
 (defn ->string
   "Serialize Hiccup-compatible data to a string"
