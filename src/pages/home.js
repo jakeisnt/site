750c3618d1e2315082edc5758b730ba38b4ba624
Jacob Chvatal <jake@isnt.online>
2023-10-21 23:48:36 +0200

    homepage js

---
+++ src/pages/home.js
@@ -0,0 +1,31 @@
+import { header } from '../html';
+
+const makeHomePage = () => {
+  return [
+    "body",
+    ["html",
+     header(),
+     ["body",
+     // (components/component "sidebar" {:target-path "/index.html"} nil nil [[:h1 "~"]])
+      component("Sidebar"),
+      ["div",
+       { class: 'site-body' },
+       ["main",
+        ["article",
+         { class: 'wikipage aboutMe' },
+         ["p", "Hey, I'm Jake Chvatal."],
+         ["p", "I'm a software engineer based in Stockholm, Sweden."],
+         ["p",
+          "During the day, I work at "
+          ["a", { class: 'external', href: "https://improvin.com" },
+           "Improvin'"],
+
+          ", building tools to help food companies reduce their environmental impact."],
+         ["p",
+          "On nights and weekends, I ",
+          ["a", {class: "internal",  href: "/pages/index.html"}, "write"],
+         ", take ",
+          ["a", {class: "external",  href: "https://instagram.com/jakeisnt"}, "photos"],
+         ", and design simple hardware and software tools."],
+         component("LastFM"),]]]]]];
+}
