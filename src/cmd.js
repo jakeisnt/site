0cddd7d16319841a1d8675b1529c519878e2a81e
Jacob Chvatal <jake@isnt.online>
2023-10-21 18:26:16 +0200

    deprecate command.clj

--- src/cmd.js
+++ src/cmd.js
@@ -1,4 +1,7 @@
-const runCommand = (command, args) => {
+// command execution made stupid simple
+
+// exec a command synchronously
+const exec = (command, args) => {
   const child = spawn(command, args, { stdio: 'inherit' });
   return new Promise((resolve, reject) => {
     child.on('close', (code) => {
@@ -11,4 +14,18 @@ const runCommand = (command, args) => {
   });
 };
 
-export { runCommand };
+// exec a command asynchronously
+const execAsync = (command, args) => {
+  const child = spawn(command, args, { stdio: 'inherit' });
+  return new Promise((resolve, reject) => {
+    child.on('close', (code) => {
+      if (code !== 0) {
+        reject(new Error(`${command} ${args.join(' ')} failed`));
+        return;
+      }
+      resolve();
+    });
+  });
+};
+
+export { exec, execAsync };
