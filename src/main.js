396d72be37e5aab7557e3a8c0156d0e4f3d4eb92
Jacob Chvatal <jake@isnt.online>
2023-10-22 21:07:28 +0200

    delet unused

--- src/main.js
+++ src/main.js
@@ -8,42 +8,6 @@ import { singleFileServer } from './server';
 
 const currentRepo = "/home/jake/site";
 
-// function deployWebsite() {
-//   deploy({ currentRepo, targetDir, deploymentBranch });
-// }
-
-// compile the full website
-// function compileWebsite() {
-//   // pull in website config
-//   const { target: targetDir, source } = website;
-
-//   // for each website source:
-//   const compiledSite = website.sources.map(({ dir, paths }) =>
-//     // for each path within that source:
-//     paths.map((pathConfig) => {
-//       // compile all of the files at that path.
-//       fileType.toDisk(
-//         compileWikiPath(
-//           { ...pathConfig, websiteTarget: targetDir },
-//           source.dir,
-//
-//         ));
-//     }
-//     ));
-
-//   // now that we have all of that info, compile the home page.
-//   compileHomePage(targetDir);
-
-//   // when should the files be written to disk?
-//   //
-//   // thinking about a more OO approach:
-//   // File is a class that has methods
-
-//   // re/record the build timestamp.
-//   // TODO: these should be recorded per-source instead.
-//   recordLastTimestamp(targetDir);
-// }
-
 // Serve a file from a particular path.
   // Supports hot reloading.
 const serve = (paths) => {
