d8612e029f2d96211ee56210e0f2ffe9a47c8ba3
Jacob Chvatal <jake@isnt.online>
2023-10-23 20:49:15 +0200

    support new filetype to show we arent hardcoded

---
+++ src/file/filetype/org.js
@@ -0,0 +1,7 @@
+import { SourceFile } from '../classes';
+
+class OrgFile extends SourceFile {
+  static filetypes = ['org'];
+}
+
+export default OrgFile;
--- src/file/filetype/scss.js
+++ src/file/filetype/scss.js
@@ -1,14 +1,7 @@
 import { SourceFile } from '../classes';
-import { exec } from '../../utils/cmd';
 
 class SCSSFile extends SourceFile {
   static filetypes = ['scss', 'sass'];
-
-  // use  'sass' to write this file as css
-  write() {
-    const sourcePath = this.path;
-    exec(`sass ${sourcePath} ${targetPath}`);
-  }
 }
 
 export default SCSSFile;
