40faaec806c769d43b543d4f957a5f89ec7dfd48
Jacob Chvatal <jake@isnt.online>
2023-10-26 22:19:31 +0200

    try adding test ts file

--- src/file/filetype/js.js
+++ src/file/filetype/js.js
@@ -1,10 +1,43 @@
 import { SourceFile } from '../classes';
+import * as ts from "typescript";
+
+const tsToJs = (tsText) => {
+  const options = { compilerOptions: { module: ts.ModuleKind.CommonJS }};
+  return ts.transpileModule(source, options).outputText;
+}
 
 class JavascriptFile extends SourceFile {
-  static filetypes = ['js', 'jsx', 'ts', 'tsx'];
+  static filetypes = ['js'];
 
   require() {
     return require(this.path.toString());
+  }
+
+  // override the create behavior to create the parent
+  static create(filePath) {
+    if (filePath.exists()) {
+      return new JavascriptFile(filePath);
+    }
+
+    // if this file doesn't exist, try making the typescript file.
+    const tsPath = filePath.replaceExtension('ts');
+
+    const newFile = readFile(tsPath);
+    const sourceFile = newFile.clone();
+
+    Object.defineProperty(sourceFile, 'text', {
+      get() {
+        return tsToJs(newFile.text);
+      }
+    });
+
+    Object.defineProperty(sourceFile, 'mimeType', {
+      get() {
+        return 'text/javascript';
+      }
+    });
+
+    return sourceFile;
   }
 }
 
---
+++ src/file/filetype/ts.js
@@ -0,0 +1,7 @@
+import { SourceFile } from '../classes';
+
+class TypescriptFile extends SourceFile {
+  static filetypes = ['ts'];
+}
+
+export default TypescriptFile;
