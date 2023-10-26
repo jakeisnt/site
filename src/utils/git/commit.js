5077b91307fb36ea54c11e3edfb24861eb5dae08
Jacob Chvatal <jake@isnt.online>
2023-10-26 06:59:55 +0200

    fix bugs with commit access

--- src/utils/git/commit.js
+++ src/utils/git/commit.js
@@ -1,25 +1,30 @@
 // a single commit in a repository
 // ask me questions about the specific commit
 class RepoCommit {
-  constructor({ shortHash, longHash, commitDate, timestamp }) {
+  constructor({ shortHash, longHash, commitDate, timestamp, repo }) {
     this.shortHash = shortHash;
     this.longHash = longHash;
     this.commitDate = commitDate;
     this.timestamp = timestamp;
+    this.repo = repo;
   }
 
   static create(args) {
     return new RepoCommit(args);
   }
 
+  get date() {
+    return this.commitDate;
+  }
+
   // get the link associated with this repo commit at a given path
   // TODO not done yet
-  historyLink(path) {
-    if (!this.remoteUrl) {
+  historyLink(filePath) {
+    if (!this.repo.remoteUrl) {
       throw new Error('Cannot create history link without a defined remote URL');
     }
 
-    return `${this.remoteUrl}/blob/${longHash}/${filePath.toString()}`;
+    return `${this.repo.remoteUrl}/blob/${this.ongHash}/${filePath.toString()}`;
   }
 
 }
