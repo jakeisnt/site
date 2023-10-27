cabe4cf517d79d5b45f2c136d5eb86b62451a545
Jacob Chvatal <jake@isnt.online>
2023-10-28 01:22:30 +0200

    reminder to me to cache things

--- src/build.js
+++ src/build.js
@@ -4,6 +4,9 @@ const build = (websiteSpec) => {
   // 2. Find all of the URLs on the file.
   // 3. Convert those URLs into files.
   // 4. Repeat until we have all of the files.
+  //
+  // Also, reading lots of files / mostly the commits
+  // is slow, so make sure to cache everythign.
 }
 
 export { build };
