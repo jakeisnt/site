944fd1c48f629578d533943a3b4df3ee1f62f297
Jacob Chvatal <jake@isnt.online>
2023-10-28 01:20:55 +0200

    plan the build strategy

---
+++ src/build.js
@@ -0,0 +1,9 @@
+const build = (websiteSpec) => {
+  // stupid strategy: pretend we are a server.
+  // 1. Get the root file's HTML.
+  // 2. Find all of the URLs on the file.
+  // 3. Convert those URLs into files.
+  // 4. Repeat until we have all of the files.
+}
+
+export { build };
