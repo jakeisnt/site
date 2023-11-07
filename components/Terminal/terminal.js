9a2818f3d96dc628cfa394651544b1386fcef434
Jacob Chvatal <jake@isnt.online>
2023-11-07 23:02:52 +0100

    ??

--- components/Terminal/terminal.js
+++ components/Terminal/terminal.js
@@ -1,18 +1,13 @@
-// ideas:
-// - browse files available
-// - open new windows with animations
-// - open files in new windows
-// - use functions remotely
-// - control things in my real life
-// - browse/analyze/visualize my personal data
+// an interactive Terminal in the browser
+const Terminal = () => ({
+  dependsOn: [
+    { src: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.js" },
+    { src: "https://cdn.jsdelivr.net/gh/jcubic/jquery.terminal@devel/js/jquery.terminal.min.js" },
+    { src: "https://cdn.jsdelivr.net/gh/jcubic/jquery.terminal@devel/css/jquery.terminal.min.css" },
+    { src: "/components/terminal/terminal.js" },
+    { src: "/components/terminal/terminal.css" },
+  ],
+  body: ["div", { class: 'terminal git-hist-table', id: 'terminal' }],
+});
 
-// biggest problem: this thing goes away when i browse to another page
-
-(() => {
-  $('.terminal').terminal(function(command, term) {
-    term.pause();
-    term.echo("I can't do much yet.").resume();
-  }, {
-    greetings: 'Welcome to `jake.isnt.online`.'
-  });
-})();
+export default Terminal;
