4c072a57991d24218c55b98afa0bfd7df9289521
Jacob Chvatal <jake@isnt.online>
2024-01-27 19:24:10 +0100

    don't hard error when trying to write afile we don't yet support

--- src/file/classes/file.js
+++ src/file/classes/file.js
@@ -73,9 +73,13 @@ class File {
   }
 
   write() {
-    throw new Error(
+    console.error(
       `file.write() is not implemented for file at '${this.path.toString()}'`
     );
+
+    // throw new Error(
+    //   `file.write() is not implemented for file at '${this.path.toString()}'`
+    // );
   }
 
   // get the url to the html page with this file
