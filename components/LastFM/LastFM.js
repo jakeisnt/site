78e52e272c8361136cf3f5efa71bb1c8bc196633
Jake Chvatal <jake@isnt.online>
2024-01-28 13:36:07 +0100

    Delete duplicate components/LastFM/LastFM.js

--- components/LastFM/LastFM.js
+++
@@ -1,6 +0,0 @@
-const LastFM = () => ({
-  dependsOn: [{ src: '/components/LastFM/lastfm.js' }, { src: '/components/LastFM/lastfm.css' }],
-  body: ["div", { class: 'lastfm-now-playing-box' }]
-})
-
-export default LastFM;
