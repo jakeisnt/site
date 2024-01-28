2910adc481a8512a6c4e253e1e88f964244418a5
Jake Chvatal <jake@isnt.online>
2024-01-28 13:36:26 +0100

    Delete duplicate components/Neko/Neko.js

--- components/Neko/Neko.js
+++
@@ -1,8 +0,0 @@
-const Neko = () => ({
-  dependsOn: [{ src: './neko.js' }, { src: './neko.css' }],
-  body: ["span",
-         ["div", { class: "neko-bed" }, ""],
-         ["div", { class: "neko" }, ""]],
-})
-
-export default Neko;
