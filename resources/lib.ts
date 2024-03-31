45df083acf391827ee49ff14a6aa77944d1f82da
Jacob Chvatal <jake@isnt.online>
2024-03-31 15:43:19 +0200

    lib tsc

--- resources/lib.ts
+++ resources/lib.ts
@@ -1,14 +1,27 @@
-function print(...txt) {
+function print(...txt: any[]) {
   return console.log(txt);
-};
+}
 
-function runOnDesktop(fn) {
+function runOnDesktop(fn: Function) {
   if (window.matchMedia("(min-width: 501px)")) {
     return fn();
   }
 }
 
-function create(elementName, attributes, parent) {
+type ElementNames = "img" | "a" | "h1";
+
+/**
+ * Create an html element with attributes and append it to a parent element
+ * @param elementName
+ * @param attributes
+ * @param parent
+ * @returns
+ */
+function create(
+  elementName: ElementNames,
+  attributes: { [key: string]: any },
+  parent: HTMLElement
+) {
   const elem = document.createElement(elementName);
   for (let key in attributes) {
     // some things only work one way, so we do both
@@ -34,7 +47,7 @@ function create2(elementName, attributes, ...children) {
   }
 
   if (children) {
-    children.forEach(child => {
+    children.forEach((child) => {
       if (typeof child === "string") {
         elem.appendChild(document.createTextNode(child));
       } else {
@@ -46,6 +59,8 @@ function create2(elementName, attributes, ...children) {
   return elem;
 }
 
+var httpRequest: XMLHttpRequest;
+
 function req(url, method, then) {
   if (window.XMLHttpRequest) {
     httpRequest = new XMLHttpRequest();
@@ -53,14 +68,14 @@ function req(url, method, then) {
     httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
   }
 
-  httpRequest.onreadystatechange = function() {
+  httpRequest.onreadystatechange = function () {
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
       if (httpRequest.status === 200) {
         // All set
         var response = JSON.parse(httpRequest.responseText);
         then(response);
       } else {
-        console.log('There was a problem with the last.fm request.');
+        console.log("There was a problem with the last.fm request.");
       }
     }
   };
@@ -69,15 +84,15 @@ function req(url, method, then) {
   httpRequest.send();
 }
 
-const get = (url, then) => req(url, 'GET', then);
+const get = (url, then) => req(url, "GET", then);
 
 const $ = (selector) => document.querySelector(selector);
 const all = (selector) => Array.from(document.querySelectorAll(selector));
 
 function dynLoad(src, id) {
-  var s = document.createElement('script');
-  s.setAttribute('src', src);
-  s.setAttribute('id', id);
-  s.setAttribute('async', true);
+  var s = document.createElement("script");
+  s.setAttribute("src", src);
+  s.setAttribute("id", id);
+  s.setAttribute("async", "true");
   return s;
-};
+}
