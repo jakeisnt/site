2dbfc45d1fe9ab301a0ffacdf87b5b669aad3b9b
Jacob Chvatal <jake@isnt.online>
2024-04-01 18:49:58 +0200

    more

--- resources/lib.ts
+++ resources/lib.ts
@@ -4,7 +4,10 @@ function print(...txt: any[]) {
   return console.log(txt);
 }
 
-function runOnDesktop(fn: Function) {
+/**
+ * Run the provided function on a desktop.
+ */
+function runOnDesktop(fn: () => void) {
   if (window.matchMedia("(min-width: 501px)")) {
     return fn();
   }
@@ -37,9 +40,10 @@ function create(
 function create2(
   elementName: HtmlTag,
   attributes: HtmlAttributes,
-  ...children
+  ...children: HTMLElement[]
 ) {
   const elem = document.createElement(elementName);
+
   for (let key in attributes) {
     // some things only work one way, so we do both
     // is this faster than a switch statement? not sure.
@@ -89,10 +93,14 @@ function req(url: string, method: HttpMethod, then: Function) {
 
 const get = (url: string, then: Function) => req(url, "GET", then);
 
-const $ = (selector) => document.querySelector(selector);
-const all = (selector) => Array.from(document.querySelectorAll(selector));
+const $ = (selector: string) => document.querySelector(selector);
+const all = (selector: string) =>
+  Array.from(document.querySelectorAll(selector));
 
-function dynLoad(src, id) {
+/**
+ * Dynamically load a script provided the src and id.
+ */
+function dynLoad(src: string, id: string) {
   var s = document.createElement("script");
   s.setAttribute("src", src);
   s.setAttribute("id", id);
