504425b389abc551836e24cc05a55336e2830180
Jacob Chvatal <jake@isnt.online>
2024-04-13 04:54:24 +0200

    capital names ew

--- components/lastfm/lastfm.ts
+++
@@ -1,9 +0,0 @@
-const LastFM = () => ({
-  dependsOn: [
-    { src: "/components/LastFM/getCurrentTrack.js", type: "module" },
-    { src: "/components/LastFM/lastfm.css" },
-  ],
-  body: ["div", { class: "lastfm-now-playing-box" }],
-});
-
-export default LastFM;
--- components/neko/neko.ts
+++
@@ -1,8 +0,0 @@
-const Neko = () => ({
-  dependsOn: [{ src: './spawnNeko.js' }, { src: './neko.css' }],
-  body: ["span",
-         ["div", { class: "neko-bed" }, ""],
-         ["div", { class: "neko" }, ""]],
-})
-
-export default Neko;
