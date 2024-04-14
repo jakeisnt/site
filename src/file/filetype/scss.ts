65b043ae0a40d8029ade84c07e4ad1e24fde85d6
Jacob Chvatal <jake@isnt.online>
2024-04-14 16:56:32 +0200

    change the css/scss file to the new infrastructure

--- src/file/filetype/scss.ts
+++ src/file/filetype/scss.ts
@@ -1,7 +1,40 @@
 import { SourceFile } from "file/classes";
 
+import { sourceDir } from "../../constants";
+import * as sass from "sass";
+const { pathToFileURL } = require("url");
+import { wrapFile } from "../classes/utils";
+import type CSSFile from "./css";
+
+/**
+ * Convert provided SCSS text to a CSS string.
+ */
+const scssToCss = (scssFile: SourceFile) => {
+  const result = sass.compileString(scssFile.text, {
+    sourceMap: false,
+    importers: [
+      {
+        findFileUrl(url) {
+          const nextUrl = new URL(url, pathToFileURL(`${sourceDir}/`));
+          return nextUrl;
+        },
+      },
+    ],
+  });
+
+  return result.css.toString();
+};
+
 class SCSSFile extends SourceFile {
-  static filetypes = ["scss", "sass"];
+  public static filetypes = ["scss", "sass"];
+  public static targets = ["css"];
+
+  css() {
+    return wrapFile(this, scssToCss, this.path, {
+      extension: "css",
+      mimeType: "text/css",
+    }) as CSSFile;
+  }
 }
 
 export default SCSSFile;
