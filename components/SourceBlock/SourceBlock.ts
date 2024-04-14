67b44ef44b430b06ca4d49bd5b994e926badc605
Jacob Chvatal <jake@isnt.online>
2024-04-14 03:50:22 +0200

    drop the utils/ file, pull in `escapeHTML`

--- components/SourceBlock/SourceBlock.ts
+++ components/SourceBlock/SourceBlock.ts
@@ -1,5 +1,5 @@
 import TextFile from "../../src/file/classes/text";
-import { escapeHtml } from "../../src/html/utils";
+import { escapeHTML } from "bun";
 import treeSitter from "tree-sitter-highlight";
 
 const SourceBlock = ({ file }: { file: TextFile }) => ({
@@ -25,7 +25,7 @@ const SourceBlock = ({ file }: { file: TextFile }) => ({
         { class: `language-${file.extension} has-raw-code` },
         language
           ? treeSitter.highlight(file.text, language)
-          : escapeHtml(file.text),
+          : escapeHTML(file.text),
       ],
     ];
   })(),
