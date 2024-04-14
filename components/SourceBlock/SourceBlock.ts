1d20ca0cfd38cb92becf398e183e9c16ea9caf82
Jacob Chvatal <jake@isnt.online>
2024-04-14 02:47:27 +0200

    move the source block css into its folder

--- components/SourceBlock/SourceBlock.ts
+++ components/SourceBlock/SourceBlock.ts
@@ -1,10 +1,13 @@
-import type { PageSettings } from "../../src/types/site";
 import TextFile from "../../src/file/classes/text";
 import { escapeHtml } from "../../src/html/utils";
 import treeSitter from "tree-sitter-highlight";
 
 const SourceBlock = ({ file }: { file: TextFile }) => ({
-  dependsOn: [],
+  dependsOn: [
+    {
+      src: "/components/SourceBlock/source-block.scss",
+    },
+  ],
   body: (() => {
     // This is a quick way to snag the language;
     // we access the enum member with an uppercase version of the file extension.
