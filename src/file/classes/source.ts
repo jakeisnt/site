24e52c5ea9149cb1afad9fb0d9a37418a89166ae
Jacob Chvatal <jake@isnt.online>
2024-04-18 16:33:37 +0200

    drop `SourceFile.asHtml` -> there is no difference

--- src/file/classes/source.ts
+++ src/file/classes/source.ts
@@ -1,17 +1,8 @@
 import TextFile from "./text";
-import { HtmlPage } from "../../html";
-import type { PageSettings } from "../../types/site";
 
 /**
  * A source code file.
  */
-class SourceFile extends TextFile {
-  asHtml(settings: PageSettings) {
-    return HtmlPage.create(
-      ["Article", { file: this, ...settings }, ["SourceBlock", { file: this }]],
-      settings
-    );
-  }
-}
+class SourceFile extends TextFile {}
 
 export default SourceFile;
