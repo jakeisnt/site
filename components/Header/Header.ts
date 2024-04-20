c8b854a16d55dacab3faa7bcd205a4fb91a1fce9
Jacob Chvatal <jake@isnt.online>
2024-04-20 17:21:13 +0200

    add meta viewport header tag

--- components/Header/Header.ts
+++ components/Header/Header.ts
@@ -90,6 +90,10 @@ const header = ({
   return [
     "head",
     ["meta", { charset: "utf-8" }],
+    [
+      "meta",
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+    ],
     ["title", `${title} / ${siteName}`],
     openGraphHeaders({ title, url, siteName }),
     meta("keywords", "jake"),
